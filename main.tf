da1c0952da792e260ce0252ccff4a7ce16e8fee0
kishan <kishanshahiete@gmail.com>
2021-02-28 01:47:23 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "hello-world" {
+
+ ami = "ami-0a313d98716f372" 
+ instance_type = "t2.micro"
+ vpc_security_group_ids = ["${aws_security_group.webserver_sg.id}"]
+ key_name = "terraform"
+ tags = {
+	 Name = "Hello world"
+ }
+
+}
+
+
+terraform { 
+   backend "s3" {
+
+      region = 
+      key = bucket name/anypath
+      bucket = bucket name
+   }
+
+}
